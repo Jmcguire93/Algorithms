b23e72c616b7973cfab5990467c95b52d30c7d22
James <james.ryan.mcguire@gmail.com>
2021-11-22 16:40:57 -0500

    Solve formate_name with while loop

--- enumerables-and-ranges/format_name.rb
+++ enumerables-and-ranges/format_name.rb
@@ -1,12 +1,34 @@
 # Format Name
 # Write a method format_name that takes in a name string and returns the name properly capitalized.
 
+# Each Loop
+
 def format_name(name)
   parts = name.split(" ")
   formatted = []
 
   parts.each { |part| formatted << part[0].upcase + part[1..-1].downcase }
 
+  formatted.join(" ")
+end
+
+p format_name("chase WILSON") # => "Chase Wilson"
+p format_name("brian CrAwFoRd scoTT") # => "Brian Crawford Scott"
+
+
+
+# While Loop
+
+def format_name(name)
+  parts = name.split(" ")
+  formatted = []
+  i = 0
+
+  while i < parts.length
+    char = parts[i] 
+    formatted << char[0].upcase + char[1..-1].downcase  
+    i += 1
+  end
   formatted.join(" ")
 end
 
---
+++ enumerables-and-ranges/is_valid_name?.rb
@@ -0,0 +1,18 @@
+# Is Valid Name
+# Write a method is_valid_name that takes in a string and returns a boolean indicating whether or not it is a valid name.
+
+# A name is valid is if satisfies all of the following:
+# - contains at least a first name and last name, separated by spaces
+# - each part of the name should be capitalized
+#
+# Hint: use str.upcase or str.downcase
+# "a".upcase # => "A"
+
+def is_valid_name?(string)
+
+end
+
+p is_valid_name("Kush Patel")       # => true
+p is_valid_name("Daniel")           # => false
+p is_valid_name("Robert Downey Jr") # => true
+p is_valid_name("ROBERT DOWNEY JR") # => false
