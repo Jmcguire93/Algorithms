566917a97579ad2b9a8d70aacd14b2a7df11c704
James <james.ryan.mcguire@gmail.com>
2021-11-22 13:47:21 -0500

    Solves count_a with while loop

--- loops/count_a.rb
+++ loops/count_a.rb
@@ -2,7 +2,7 @@
 # Write a method count_a(word) that takes in a string word and returns the number of a's in the word. 
 # The method should count both lowercase (a) and uppercase (A).
 
-#Each Loops
+#Each Loop
 
 def count_a(word)
   count = 0
@@ -11,6 +11,29 @@ def count_a(word)
     if char == "a" || char == "A"
       count += 1
     end
+  end
+  count 
+end
+
+p count_a("application")  # => 2
+p count_a("bike")         # => 0
+p count_a("Arthur")       # => 1
+p count_a("Aardvark")     # => 3
+
+
+#While Loop
+
+def count_a(word)
+  count = 0
+  i = 0
+
+  while i < word.length 
+    char = word[i]
+    if char == "a" || char == "A"
+      count += 1
+    end
+
+    i += 1
   end
   count 
 end
