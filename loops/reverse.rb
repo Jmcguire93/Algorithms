16aefe26a422f8672644dbc78334b81d04299d20
James <james.ryan.mcguire@gmail.com>
2021-11-22 14:46:25 -0500

    Solve reverse with each loop

--- loops/reverse.rb
+++ loops/reverse.rb
@@ -1,6 +1,8 @@
 # Reverse
 # Write a method reverse(word) that takes in a string word and returns the word with its letters in reverse order.
 
+# While Loop
+
 def reverse(word)
   reversed = ""
   i = word.length - 1
@@ -9,6 +11,23 @@ def reverse(word)
     reversed << word[i]  
     i -= 1
   end
+  reversed 
+end
+
+p reverse("cat")          # => "tac"
+p reverse("programming")  # => "gnimmargorp"
+p reverse("bootcamp")     # => "pmactoob"
+
+
+# Each Loop
+
+def reverse(word)
+  reversed = ""
+  
+  word.reverse.each_char do |char|
+    reversed += char 
+  end
+
   reversed 
 end
 
