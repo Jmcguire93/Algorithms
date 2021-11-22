9c247c81da794297f189f0c95fe9e5067d93e617
James <james.ryan.mcguire@gmail.com>
2021-11-22 13:57:02 -0500

    Solve sum_nums with each loop

--- loops/sum_nums.rb
+++ loops/sum_nums.rb
@@ -1,6 +1,8 @@
 # Sum Nums
 # Write a method sum_nums(max) that takes in a number max and returns the sum of all numbers from 1 up to and including max.
 
+# While Loop
+
 def sum_nums(max)
   sum = 0
   i = 1
@@ -9,6 +11,20 @@ def sum_nums(max)
     sum += i 
     i += 1
   end
+  sum 
+end
+
+p sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
+p sum_nums(5) # => 15
+
+# Each Loop
+
+def sum_nums(max)
+  sum = 0
+  i = 1
+
+  (i..max).each { |num| sum += num } 
+
   sum 
 end
 
