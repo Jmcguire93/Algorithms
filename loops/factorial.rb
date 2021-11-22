41e3fd89a955b05f271b3e7135ab5156e77b9297
James <james.ryan.mcguire@gmail.com>
2021-11-22 14:12:30 -0500

    Solve factorial with while loop

--- loops/factorial.rb
+++ loops/factorial.rb
@@ -1,6 +1,8 @@
 # Factorial
 # Write a method factorial(num) that takes in a number num and returns the product of all numbers from 1 up to and including num.
 
+# Each Loop
+
 def factorial(num)
   product = 1
 
@@ -8,6 +10,22 @@ def factorial(num)
     product *= number 
   end
 
+  product 
+end
+
+p factorial(3) # => 6, because 1 * 2 * 3 = 6
+p factorial(5) # => 120, because 1 * 2 * 3 * 4 * 5 = 120
+
+# While Loop
+
+def factorial(num)
+  product = 1
+  i = 1
+
+  while i <= num
+    product *= i 
+    i += 1
+  end
   product 
 end
 
