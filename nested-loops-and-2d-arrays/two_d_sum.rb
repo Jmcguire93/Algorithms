b275686c8dc3dba7b199d888facc0b157ff9a90c
James <james.ryan.mcguire@gmail.com>
2022-01-21 09:49:47 -0500

    Solve two_d_sum

---
+++ nested-loops-and-2d-arrays/two_d_sum.rb
@@ -0,0 +1,20 @@
+# Two D Sum
+
+# Write a method two_d_Sum that takes in a two dimensional array and returns the sum of all elements in the array.
+
+def two_d_sum(arr)
+  return arr.flatten.sum
+end
+
+array1 = [
+  [4, 5],
+  [1, 3, 7, 1]
+]
+puts two_d_sum(array1)    # => 21
+
+array2 = [
+  [3, 3],
+  [2],
+  [2, 5]
+]
+puts two_d_sum(array2)    # => 15
