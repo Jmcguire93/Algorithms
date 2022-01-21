76b7651ba70fc6296a79d34128800083e015a4d2
James <james.ryan.mcguire@gmail.com>
2022-01-21 09:52:02 -0500

    Add second solution for two_d_sum

--- nested-loops-and-2d-arrays/two_d_sum.rb
+++ nested-loops-and-2d-arrays/two_d_sum.rb
@@ -18,3 +18,31 @@ array2 = [
   [2, 5]
 ]
 puts two_d_sum(array2)    # => 15
+
+## Second solution
+
+def two_d_sum(arr)
+  sum = 0 
+
+  arr.each do |sub_arr|
+    sub_arr.each do |num|
+      sum += num 
+    end
+  end
+
+  sum
+  
+end
+
+array1 = [
+  [4, 5],
+  [1, 3, 7, 1]
+]
+puts two_d_sum(array1)    # => 21
+
+array2 = [
+  [3, 3],
+  [2],
+  [2, 5]
+]
+puts two_d_sum(array2)    # => 15
