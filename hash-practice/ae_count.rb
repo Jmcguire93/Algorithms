8f9b18bac74a0bb49bc9928b4eafed86134173b5
James <james.ryan.mcguire@gmail.com>
2022-01-28 12:57:55 -0500

    Solve ae_count

---
+++ hash-practice/ae_count.rb
@@ -0,0 +1,17 @@
+# Ae Count
+# Write a method ae_count that takes in a string and returns a hash containing the number of a's and e's in the string. 
+# Assume the string contains only lowercase characters.
+
+def ae_count(string)
+  count = {"a"=>0, "e"=>0}
+
+  string.each_char do |char|
+    if char == "a" || char == "e"
+      count[char] += 1 
+    end
+  end 
+  count 
+end
+
+p ae_count("everyone can program") #=> {"a"=>2, "e"=>3}
+p ae_count("keyboard") #=> {"a"=>1, "e"=>1}
