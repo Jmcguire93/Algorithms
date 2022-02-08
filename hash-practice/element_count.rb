d8d3a9627f03586410c81833fa75e232c889cb86
James <james.ryan.mcguire@gmail.com>
2022-02-07 19:14:03 -0500

    Solve element_count

---
+++ hash-practice/element_count.rb
@@ -0,0 +1,13 @@
+# Element Count
+
+# Write a method element_count that takes in an array and returns a hash representing the count of each element in the array.
+
+def element_count(arr)
+  count = Hash.new(0)
+
+  arr.each { |ele| count[ele] += 1 }
+  count 
+end
+
+p element_count(["a", "b", "a", "a", "b"]) #=> {"a"=>3, "b"=>2}
+p element_count(["red", "red", "blue", "green"]) #=> {"red"=>2, "blue"=>1, "green"=>1}
